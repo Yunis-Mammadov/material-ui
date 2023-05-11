eb81c85f8dce9cb104171b4c4e9fdfb7e77e3c33
Yunis-Mammadovm <yunismammadovv@gmail.com>
2023-05-11 11:39:42 +0400

    add Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,11 @@
-import { Typography } from '@mui/material'
-import React from 'react'
 
 const Footer = () => {
   return (
-    <>
-      <Typography>
-        Footer
-      </Typography>
-    </>
+    <div style={{textAlign:"center",marginTop: 100}}>
+    <h2>Footer</h2>
+    <p>Something here to give the footer a purpose!</p>
+    <p>Copyright © Your Website 2023.</p>
+    </div>
   )
 }
 
